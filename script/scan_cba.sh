bbdc4d84cd42efcd8df7a3e8efe93001c555c28b
SolidShen <solidshen519@gmail.com>
2024-09-26 01:14:06 -0400

    add test script

---
+++ script/scan_cba.sh
@@ -0,0 +1,18 @@
+gpu="1"
+
+# test cba 
+# test alpaca
+# test self-instruct
+# test four model structures
+
+python -m bait.main \
+    --base_model "meta-llama/Llama-2-7b-hf" \
+    --adapter_path "model_zoo/models/id-0000/model" \
+    --cache_dir "model_zoo/base_models" \
+    --data_dir "data" \
+    --prompt_type "val" \
+    --prompt_size 20 \
+    --gpu $gpu \
+    --output_dir "result" \
+    # --report_to "wandb"
+    
